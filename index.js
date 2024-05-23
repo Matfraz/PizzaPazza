2c4edd5c000eabd6d9e213a67109747280125fcb
Matfraz <166552676+Matfraz@users.noreply.github.com>
2024-05-23 22:37:21 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+//funzione per caricamento
+window.addEventListener('load', function() {
+    var loadingScreen = document.getElementById('loading-screen');
+    localStorage.removeItem('loadingScreenHidden'); // Rimuovi l'indicatore che la schermata è stata nascosta
+
+    if (loadingScreen) {
+        setTimeout(function() {
+            loadingScreen.style.display = 'none';
+        }, 2000);
+    }
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let item = document.querySelector('.hamburger'); // Seleziona l'elemento hamburger
+    if (item) { // Verifica che l'elemento esista
+        item.addEventListener("click", function() { // Aggiunge un ascoltatore di eventi per il click
+            document.querySelector('.nav-links').classList.toggle('open'); // Aggiunge/toglie la classe 'open' all'elemento menu
+            item.classList.toggle('open'); // Aggiunge/toglie la classe 'open' all'elemento hamburger
+        });
+    } else {
+        console.error("Elemento .hamburger non trovato");
+    }
+});
+
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
 window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
@@ -68,3 +100,35 @@ function prevSlide() {
 
 setInterval(nextSlide, 3000); // Cambia slide ogni 3 secondi
 
+
+function openPopup() {
+    document.getElementById("history-popup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("history-popup").style.display = "none";
+}
+
+let slideInde = 0;
+showSlides(slideInde);
+
+function nextSlide() {
+    showSlides(slideInde += 1);
+}
+
+function prevSlide() {
+    showSlides(slideInde -= 1);
+}
+
+function showSlides(n) {
+    const slides = document.querySelectorAll('.carousel-slide img');
+    if (n >= slides.length) {
+        slideInde = 0;
+    }
+    if (n < 0) {
+        slideInde = slides.length - 1;
+    }
+    slides.forEach((slide, index) => {
+        slide.style.display = index === slideInde ? 'block' : 'none';
+    });
+}
